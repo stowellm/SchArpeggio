ab27b01c5ac5396c9176bc2eb2acc9dab20a82d8
Mike <mike@os306-imac-2.local>
2014-04-18 11:03:37 -0400

    fixed some issues in driver because we are good at error checking, added proj outline to repo

--- driver.rkt
+++ driver.rkt
@@ -24,6 +24,7 @@
 
 ;; #region Running the driver
 
+
 ; procedure to start the driver up
 (define (run)
   (define chord (make-chord (ask-for-chord)))
--- objects.rkt
+++ objects.rkt
@@ -1,6 +1,6 @@
 ; NOTE - remove this line when ready to port over.  
 ; Uncomment when developing to avoid errors
-;#lang racket
+#lang racket
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; SchArpeggio                                          ;;;
@@ -66,9 +66,14 @@
 
 ; convert the string input to our internal chord list
 (define (string->chord str)
-  (map string->symbol 
+  (map note-sym-to-chord-obj 
+    (map string->symbol 
        (map notes-to-g3-through-f4 
-            (string-split str)))
+            (string-split str))))
+)
+
+(define (note-sym-to-chord-obj sym)
+   (hash-ref note-with-name sym)
 )
   
 ; convert notes to appropriate representation
@@ -166,7 +171,7 @@
 ;; #region Objects by Mike and Jeremy
 
 ; Note definition
-(define (make-note note frequency position height sharp flag-pos-up)
+(define (make-note name frequency position height sharp flag-pos-up)
   (define (dispatch msg)
     (cond ((eq? msg 'name) name)
           ((eq? msg 'freq) frequency)
@@ -194,79 +199,66 @@
 ; Mike - give notes a draw height
 (define (make-draw-height-itr)
   (define n 145)
-  (define (dispatch)
+  (define (dispatch . msg)
     (cond ((and (pair? msg) (eq? (car msg) 'sharp)) n)
           (else (begin (set! n (- n 5))
                        n))))
   dispatch)
 (define cur-draw-height (make-draw-height-itr))
 
-; Mike - determine if this note has a flag up
-(define (has-flag-up? note)
-  (let ((height ((hash-ref note-with-name note) 'height)))
-    (cond ((and (> height 100) (< height 145) #f))
-          ((and (> height 75)  (< height 105) #t))
-          ((and (> height 35)  (< height 80) #f))
-          (else #t)
-    )
-  )
-)
-
 ; Notes themselves (Jer and Mike)
 (define note-with-name (hash
-                'c2 (make-note  'c2    65.40 (cur-position) (cur-draw-height)        #f (has-flag-up? 'c2))
-               'c#2 (make-note 'c#2    69.29 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'c#2))
-                'd2 (make-note  'd2    73.41 (cur-position) (cur-draw-height)        #f (has-flag-up? 'd2))
-               'd#2 (make-note 'd#2    77.78 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'd#2))
-                'e2 (make-note  'e2    82.40 (cur-position) (cur-draw-height)        #f (has-flag-up? 'e2))
-                'f2 (make-note  'f2    87.30 (cur-position) (cur-draw-height)        #f (has-flag-up? 'f2))
-               'f#2 (make-note 'f#2    92.49 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'f#2))
-                'g2 (make-note  'g2    97.99 (cur-position) (cur-draw-height)        #f (has-flag-up? 'g2))
-               'g#2 (make-note 'g#2   103.82 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'g#2))
-                'a2 (make-note  'a2   110.00 (cur-position) (cur-draw-height)        #f (has-flag-up? 'a2))
-               'a#2 (make-note 'a#2   116.54 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'a#2))
-                'b2 (make-note  'b2   123.47 (cur-position) (cur-draw-height)        #f (has-flag-up? 'b2))
-                'c3 (make-note  'c3   130.81 (cur-position) (cur-draw-height)        #f (has-flag-up? 'c3))
-               'c#3 (make-note 'c#3   138.59 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'c#3))
-                'd3 (make-note  'd3   146.83 (cur-position) (cur-draw-height)        #f (has-flag-up? 'd3))
-               'd#3 (make-note 'd#3   155.56 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'd#3))
-                'e3 (make-note  'e3   164.81 (cur-position) (cur-draw-height)        #f (has-flag-up? 'e3))
-                'f3 (make-note  'f3   174.61 (cur-position) (cur-draw-height)        #f (has-flag-up? 'f3))
-               'f#3 (make-note 'f#3   184.99 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'f#3))
-                'g3 (make-note  'g3   195.99 (cur-position) (cur-draw-height)        #f (has-flag-up? 'g3))
-               'g#3 (make-note 'g#3   207.65 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'g#3))
-                'a3 (make-note  'a3   220.00 (cur-position) (cur-draw-height)        #f (has-flag-up? 'a3))
-               'a#3 (make-note 'a#3   233.08 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'a#3))
-                'b3 (make-note  'b3   246.94 (cur-position) (cur-draw-height)        #f (has-flag-up? 'b3))
-                'c4 (make-note  'c4   261.62 (cur-position) (cur-draw-height)        #f (has-flag-up? 'c4))
-               'c#4 (make-note 'c#4   277.18 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'c#4))
-                'd4 (make-note  'd4   293.66 (cur-position) (cur-draw-height)        #f (has-flag-up? 'd4))
-               'd#4 (make-note 'd#4   311.12 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'd#4))
-                'e4 (make-note  'e4   329.63 (cur-position) (cur-draw-height)        #f (has-flag-up? 'e4))
-                'f4 (make-note  'f4   349.22 (cur-position) (cur-draw-height)        #f (has-flag-up? 'f4))
-               'f#4 (make-note 'f#4   369.99 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'f#4))
-                'g4 (make-note  'g4   391.99 (cur-position) (cur-draw-height)        #f (has-flag-up? 'g4))
-               'g#4 (make-note 'g#4   415.30 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'g#4))
-                'a4 (make-note  'a4   440.00 (cur-position) (cur-draw-height)        #f (has-flag-up? 'a4))
-               'a#4 (make-note 'a#4   466.16 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'a#4))
-                'b4 (make-note  'b4   493.88 (cur-position) (cur-draw-height)        #f (has-flag-up? 'b4))
-                'c5 (make-note  'c5   523.25 (cur-position) (cur-draw-height)        #f (has-flag-up? 'c5))
-               'c#5 (make-note 'c#5   554.36 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'c#5))
-                'd5 (make-note  'd5   587.33 (cur-position) (cur-draw-height)        #f (has-flag-up? 'd5))
-               'd#5 (make-note 'd#5   622.25 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'd#5))
-                'e5 (make-note  'e5   659.25 (cur-position) (cur-draw-height)        #f (has-flag-up? 'e5))
-                'f5 (make-note  'f5   698.45 (cur-position) (cur-draw-height)        #f (has-flag-up? 'f5))
-               'f#5 (make-note 'f#5   739.98 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'f#5))
-                'g5 (make-note  'g5   783.99 (cur-position) (cur-draw-height)        #f (has-flag-up? 'g5))
-               'g#5 (make-note 'g#5   830.60 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'g#5))
-                'a5 (make-note  'a5   880.00 (cur-position) (cur-draw-height)        #f (has-flag-up? 'a5))
-               'a#5 (make-note 'a#5   932.32 (cur-position) (cur-draw-height 'sharp) #t (has-flag-up? 'a#5))
-                'b5 (make-note  'b5   987.76 (cur-position) (cur-draw-height)        #f (has-flag-up? 'b5))
-                'c6 (make-note  'c6  1046.50 (cur-position) (cur-draw-height)        #f (has-flag-up? 'c6))
+                'c2 (make-note  'c2    65.40 (cur-position) (cur-draw-height)        #f #t)
+               'c#2 (make-note 'c#2    69.29 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'd2 (make-note  'd2    73.41 (cur-position) (cur-draw-height)        #f #t)
+               'd#2 (make-note 'd#2    77.78 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'e2 (make-note  'e2    82.40 (cur-position) (cur-draw-height)        #f #t)
+                'f2 (make-note  'f2    87.30 (cur-position) (cur-draw-height)        #f #t)
+               'f#2 (make-note 'f#2    92.49 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'g2 (make-note  'g2    97.99 (cur-position) (cur-draw-height)        #f #t)
+               'g#2 (make-note 'g#2   103.82 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'a2 (make-note  'a2   110.00 (cur-position) (cur-draw-height)        #f #t)
+               'a#2 (make-note 'a#2   116.54 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'b2 (make-note  'b2   123.47 (cur-position) (cur-draw-height)        #f #t)
+                'c3 (make-note  'c3   130.81 (cur-position) (cur-draw-height)        #f #t)
+               'c#3 (make-note 'c#3   138.59 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'd3 (make-note  'd3   146.83 (cur-position) (cur-draw-height)        #f #f)
+               'd#3 (make-note 'd#3   155.56 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'e3 (make-note  'e3   164.81 (cur-position) (cur-draw-height)        #f #f)
+                'f3 (make-note  'f3   174.61 (cur-position) (cur-draw-height)        #f #f)
+               'f#3 (make-note 'f#3   184.99 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'g3 (make-note  'g3   195.99 (cur-position) (cur-draw-height)        #f #f)
+               'g#3 (make-note 'g#3   207.65 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'a3 (make-note  'a3   220.00 (cur-position) (cur-draw-height)        #f #f)
+               'a#3 (make-note 'a#3   233.08 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'b3 (make-note  'b3   246.94 (cur-position) (cur-draw-height)        #f #f)
+                'c4 (make-note  'c4   261.62 (cur-position) (cur-draw-height)        #f #f)
+               'c#4 (make-note 'c#4   277.18 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'd4 (make-note  'd4   293.66 (cur-position) (cur-draw-height)        #f #f)
+               'd#4 (make-note 'd#4   311.12 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'e4 (make-note  'e4   329.63 (cur-position) (cur-draw-height)        #f #t)
+                'f4 (make-note  'f4   349.22 (cur-position) (cur-draw-height)        #f #t)
+               'f#4 (make-note 'f#4   369.99 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'g4 (make-note  'g4   391.99 (cur-position) (cur-draw-height)        #f #t)
+               'g#4 (make-note 'g#4   415.30 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'a4 (make-note  'a4   440.00 (cur-position) (cur-draw-height)        #f #t)
+               'a#4 (make-note 'a#4   466.16 (cur-position) (cur-draw-height 'sharp) #t #t)
+                'b4 (make-note  'b4   493.88 (cur-position) (cur-draw-height)        #f #t)
+                'c5 (make-note  'c5   523.25 (cur-position) (cur-draw-height)        #f #f)
+               'c#5 (make-note 'c#5   554.36 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'd5 (make-note  'd5   587.33 (cur-position) (cur-draw-height)        #f #f)
+               'd#5 (make-note 'd#5   622.25 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'e5 (make-note  'e5   659.25 (cur-position) (cur-draw-height)        #f #f)
+                'f5 (make-note  'f5   698.45 (cur-position) (cur-draw-height)        #f #f)
+               'f#5 (make-note 'f#5   739.98 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'g5 (make-note  'g5   783.99 (cur-position) (cur-draw-height)        #f #f)
+               'g#5 (make-note 'g#5   830.60 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'a5 (make-note  'a5   880.00 (cur-position) (cur-draw-height)        #f #f)
+               'a#5 (make-note 'a#5   932.32 (cur-position) (cur-draw-height 'sharp) #t #f)
+                'b5 (make-note  'b5   987.76 (cur-position) (cur-draw-height)        #f #f)
+                'c6 (make-note  'c6  1046.50 (cur-position) (cur-draw-height)        #f #f)
                 )
 )
-
-
 
 ;; #endregion
 
