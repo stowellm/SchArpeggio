97c7c1b787bd4ebb98b5576714a9b82802987a9b
Mike <mike@os306-imac-2.local>
2014-04-25 09:36:20 -0400

    treble and bass clefs and right staves

--- drawing.rkt
+++ drawing.rkt
@@ -24,40 +24,127 @@
 (define num-staves 0)
 
 ; basic method to draw a new music staff
-(define (draw-new-staff) ; TODO - this will be a left-staff
-  (next-staff-start #t)
-  (split* 
-          ; treble clef
-          (move-offset 0 20)  (move-offset 0 30) (move-offset 0 40)
-          (move-offset 0 50)  (move-offset 0 60)
-          ; bass clef
-          (move-offset 0 80)  (move-offset 0 90) (move-offset 0 100)
-          (move-offset 0 110) (move-offset 0 120))
-  (draw 500)
-  (next-staff-start #f)
+(define (draw-new-staff . is-right-staff?) ; TODO - bool for left or right staff
+  (let ((draw-right (and (not (null? is-right-staff?)) is-right-staff?)))
+    (if draw-right
+      (next-right-staff-start #f)
+      (next-left-staff-start #t))
+    (split* 
+            ; treble clef
+            (move-offset 0 20)  (move-offset 0 30) (move-offset 0 40)
+            (move-offset 0 50)  (move-offset 0 60)
+            ; bass clef
+            (move-offset 0 80)  (move-offset 0 90) (move-offset 0 100)
+            (move-offset 0 110) (move-offset 0 120))
+    (if draw-right
+      (draw 215)
+      (draw 500))
+    (if draw-right
+      (next-right-staff-start #t)
+      (next-left-staff-start #f))
+  )
+  (draw-treble-clef)
+  (draw-bass-clef)
+)
+
+; draw the treble clef on the staff
+(define (draw-treble-clef)
+  (move-offset 0 48)
+  (draw-offset 5 -7)
+  (draw-offset 5 0)
+  (draw-offset 5 7)
+  (draw-offset -5 7)
+  (draw-offset -12 0)
+  (draw-offset -4 -7)
+  (draw-offset 22 -27)
+  (draw-offset 0 -4)
+  (draw-offset -5 -5)
+  (draw-offset -5 5)
+  (draw-offset 0 55)
+  (draw-offset -7 -7)
+  (draw-offset 5 0)
+  (move-offset -4 -61)
+)
+
+; draw the bass clef on the staff
+(define (draw-bass-clef)
+  (move-offset 4 88)
+  (draw-offset 0 5)
+  (draw-offset -3 3)
+  (draw-offset -4 0)
+  (draw-offset -3 -3)
+  (draw-offset 0 -8)
+  (draw-offset 3 -4)
+  (draw-offset 13 0)
+  (draw-offset 3 4)
+  (draw-offset 0 10)
+  (draw-offset -5 7)
+  (draw-offset -14 9)
+  (move-offset 25 -25)
+  (draw-offset 0 2)
+  (move-offset 0 2)
+  (draw-offset 0 2)
+  (move-offset -18 -92)
 )
 
 
 ; draw the notes from the progression passed in by the driver
+; on a right-sided staff
 (define (draw-progression progression)
-  ; TODO - draw a right-staff
+  ;(next-right-staff-start)
+  (draw-new-staff #t)
+  (progression-helper progression)
+  (draw-repeat)
+)
+
+(define (progression-helper progression)
   (if (null? progression)
       'progression-drawn
       (begin
         (draw-chord (car progression))
-        (draw-progression (cdr progression))
+        (progression-helper (cdr progression))
       )
   )
 )
 
-; go to the next staff starting location
+; draw the repeat symbol on the sheet
+(define (draw-repeat)
+  (home)
+  (move-offset 245 225)
+  (draw-offset 30 0)
+  (draw-offset 0 -30)
+  (draw-offset -30 0)
+  (draw-offset 0 15)
+  (draw-offset 5 -5)
+  (move-offset -5 5)
+  (draw-offset -5 -5)
+)
+
+; get the turtle off the screen so we can enjoy the music sheet
+; without an obstruction
+(define (disappear)
+  (move-offset 1000 1000)
+)
+
+; position the turtle in place to draw a right staff
+; boolean indicates whether or not to position 10 more pixels
+; over to begin drawing notes
+(define (next-right-staff-start bool)
+  (home)
+  (move-offset 165 -375)
+  (if bool
+      (move-offset 10 0)
+      (nothing))
+)
+
+; go to the next left staff starting location
 ; boolean indicates whether or not to increment num-staves
-(define (next-staff-start bool)
+(define (next-left-staff-start bool)
   (home)
   (move-offset -375 (+ -375 (* num-staves 160)))
-  (if (equal? bool #t)
+  (if bool
       (set! num-staves (+ num-staves 1))
-      (move-offset 0 -160))
+      (move-offset 10 -160))
 )
 
 ; draws a chord
@@ -68,6 +155,9 @@
   ; TODO - draw the rest of the notes
 )
 
+; method to be called either by the draw-chord procedure
+; internally or by Jeremy's arpegiator library to draw
+; notes on the staves
 (define (draw-note note speed)
   ; move the turtle in place
   (move-offset 30 (note 'height))
@@ -149,16 +239,17 @@
 (define (draw-extra-staff-line name)
   ; extra line through the note
   (cond ((or (equal? name 'c4)
+             (equal? name 'c#4)
              (equal? name 'e2)
-             (equal? name 'a5))
+             (equal? name 'a5)
+             (equal? name 'a#5))
         (line-through))
       ; extra line below the note
-      ((or (equal? name 'd4)
-           (equal? name 'b3)
-           (equal? name 'b5))
+      ((equal? name 'b5)
         (line-below))
       ; extra line above the note
-      ((equal? name 'd2)
+      ((or (equal? name 'd2)
+           (equal? name 'd#2))
         (line-above))
       ; extra line through and below the note
       ((equal? name 'c6)
@@ -167,7 +258,8 @@
           (line-far-below)
         ))
       ; extra line through and above the note
-      ((equal? name 'c2)
+      ((or (equal? name 'c2)
+           (equal? name 'c#2))
         (begin
           (line-through)
           (line-far-above)
@@ -289,17 +381,37 @@
   (draw-note (hash-ref note-with-name 'c#4) 'eighth)
   (draw-note (hash-ref note-with-name 'c2)  'quarter)
   (draw-note (hash-ref note-with-name 'd#2) 'sixteenth)
-  (draw-note (hash-ref note-with-name 'e2)  'quarter)
+  (draw-note (hash-ref note-with-name 'e2)  'half)
   (draw-note (hash-ref note-with-name 'f#2) 'eighth)
   (draw-note (hash-ref note-with-name 'g2)  'sixteenth)
-  (draw-note (hash-ref note-with-name 'c6)  'eighth)
+  (draw-note (hash-ref note-with-name 'c6)  'whole)
   (draw-note (hash-ref note-with-name 'b5)  'quarter)
-  (draw-note (hash-ref note-with-name 'a#5) 'eighth)
-  (draw-note (hash-ref note-with-name 'g5)  'quarter)
+  (draw-note (hash-ref note-with-name 'a#5) 'half)
+  (draw-note (hash-ref note-with-name 'g5)  'whole)
   (draw-note (hash-ref note-with-name 'f5)  'eighth)
   (draw-note (hash-ref note-with-name 'f#5) 'sixteenth)
   (draw-note (hash-ref note-with-name 'f5)  'eighth)
-  (draw-note (hash-ref note-with-name 'f#5) 'sixteenth)
+  (draw-note (hash-ref note-with-name 'f#5) 'half)
   (draw-note (hash-ref note-with-name 'f5)  'eighth)
 )
 
+(define (big-test)
+  (test)
+  (test)
+  (test)
+  (test)
+  ;(draw-new-staff #t)
+  (define chord1 (make-chord "d" "b" "a" "b" "c"))
+  (define chord2 (make-chord "b" "b" "b" "b" "c"))
+  (define progression (list chord1 chord2))
+  (draw-progression progression)
+  ;(draw-repeat)
+  (disappear)
+)
+
+(define (clef-test)
+  (turtles #t)
+  (draw-new-staff)
+  (draw-bass-clef)
+  (disappear)
+)
